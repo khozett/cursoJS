4a53522282935e9cdc2eb5d70763e9b1271c78d3
khozett <sergiodaconceicaosouza@gmail.com>
2023-11-10 09:46:28 -0300

    att

--- js-antes-do-react/main.js
+++ js-antes-do-react/main.js
@@ -3,18 +3,43 @@
 
 const user = {
   name: 'Sérgio',
-  idade: 18,
-  endereco: {
-    rua: 'bla',
-    numero: '234'
+  age: 18,
+  andress: {
+    street: 'bla',
+    number: '234'
   },
 
 }
 
 
+//Nullish coalescing operator
+
+
+//const idade = null;
+
+//document.body.innerText = "Sua idade é: "+ (idade ?? 'Não informado');
+
+
+//objetos
+
+
+//document.body.innerText = (
+  //'name' in user;
+  //Object.values(user);
+  //JSON.stringify(Object.values(user));
+  //JSON.stringify(Object.entries(user));
+
+//
+
 
 //desestruturação
-const { endereco, name } = user
+const { andress, name } = user
 
 
-document.body.innerText = JSON.stringify({ endereco, name });
+function mostraIdade(user){
+  return user.age
+}
+
+document.body.innerText = mostraIdade(user)
+
+//JSON.stringify({ endereco, name });
