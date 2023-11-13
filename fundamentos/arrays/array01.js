ea98f36d2ec2bcf355b8c4ebf2c5d735ce143ce7
khozett <sergiodaconceicaosouza@gmail.com>
2023-11-13 05:07:05 -0300

    att

---
+++ fundamentos/arrays/array01.js
@@ -0,0 +1,14 @@
+const peoples = ["Sergio", "João", "Maria"];
+
+console.log(peoples.length);
+console.log(peoples[2])
+
+for (let people of peoples){
+  console.log(people);
+}
+
+const numbers = [1, 5, 20, 30, 40];
+
+for (let number of numbers){
+  console.log(number);
+}
